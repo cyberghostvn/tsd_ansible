16f3ebc3a1850dfd1b44f28bbc44b48499a43c6b
Luu Nghia Nghia <luunghianghia@gmail.com>
2022-09-08 17:34:42 +0700

    Update Files new

--- roles/cron/files/cron.sh
+++ roles/cron/files/cron.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/usr/bin/bash
 rsync /backup_$(hostname) -azve ssh nghialv@10.0.6.4:/data/backup_$(date +%F)
--- roles/cron/files/cron_local.sh
+++ roles/cron/files/cron_local.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-rsync /etc/ansible -az /backup_$(hostname)
-rsync /tmp -az /backup_$(hostname)
+#!/usr/bin/bash
+sudo rsync /etc/ansible -az /backup_$(hostname)
+sudo rsync /var/log -az /backup_$(hostname)
