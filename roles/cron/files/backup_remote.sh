ec7c842c89f5bccf7d9f063e6cd1ade96cec04cf
Luu Nghia Nghia <luunghianghia@gmail.com>
2022-09-09 11:02:41 +0700

    Add Send Alert to telegram for backup_remote.sh file

--- roles/cron/files/backup_remote.sh
+++ roles/cron/files/backup_remote.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/bash
+exec > /tmp/$(hostname)_remote.log 2>&1
+echo -e "Host: $(hostname)"
 rsync /backup_$(hostname) -azve ssh nghialv@10.0.6.4:/data/backup_$(date +%F)
+TOKEN="5520385337:AAGc0Xtsw1xYVYZ0rTzpyrDx-AeVXyf6rh4"
+ID="-653736045"
+URL="https://api.telegram.org/bot$TOKEN/sendMessage"
+output=$(cat /tmp/$(hostname)_remote.log)
+curl -s -X POST $URL -d chat_id=$ID -d text="$output"
