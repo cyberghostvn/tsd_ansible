d6d148b64ee612620966c9248ba668402a454f5a
Luu Nghia Nghia <luunghianghia@gmail.com>
2022-09-09 10:10:35 +0700

    add script send alert to telegram

--- roles/cron/files/backup_local.sh
+++ roles/cron/files/backup_local.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/bash
+#!/usr/bin/bash
 sudo rsync /etc/ansible -az /backup_$(hostname)
 sudo rsync /var/log -az /backup_$(hostname)
+sudo rsync /tmp -az /backup_$(hostname)
+
+TOKEN="5520385337:AAGc0Xtsw1xYVYZ0rTzpyrDx-AeVXyf6rh4"
+ID="-653736045"
+URL="https://api.telegram.org/bot$TOKEN/sendMessage"
+#bold=$(tput bold)
+content=$(echo -e "Host: $(hostname)\n$(ls -lsh /backup_$(hostname))" > /tmp/$(hostname).log)
+output=$(cat /tmp/$(hostname).log)
+curl -s -X POST $URL -d chat_id=$ID -d text="$output"
