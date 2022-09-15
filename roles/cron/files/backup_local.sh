f53e9d7d6c76634d489f457f80186eddd306c07d
Luu Nghia Nghia <luunghianghia@gmail.com>
2022-09-15 08:33:58 +0700

    update file backup_local.sh

--- roles/cron/files/backup_local.sh
+++ roles/cron/files/backup_local.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/bash
 exec > /tmp/$(hostname).log 2>&1
-sudo rsync /etc/ansible -az /backup_$(hostname)
-sudo rsync /var/log -az /backup_$(hostname)
-#sudo rsync /tmp -az /backup_$(hostname)
-
+#sudo rsync /etc/ansible/ -az /backup_$(hostname)/ansible_$(date +%s)
+sudo rsync /var/log/ -az /backup_$(hostname)/log_$(date +%s)
+sudo rsync /var/ossec/ -az /backup_$(hostname)/ossec_$(date +%s)
+echo -e "Host: $(hostname)\n$(ls -lsht /backup_$(hostname) | awk '{ print $NF }')"
 TOKEN="5520385337:AAGc0Xtsw1xYVYZ0rTzpyrDx-AeVXyf6rh4"
 ID="-653736045"
 URL="https://api.telegram.org/bot$TOKEN/sendMessage"
-#bold=$(tput bold)
 #content=$(echo -e "Host: $(hostname)\n$(ls -lsh /backup_$(hostname))" > /tmp/$(hostname).log)
-echo -e "Host: $(hostname)\n$(ls -lsh /backup_$(hostname))"
 output=$(cat /tmp/$(hostname).log)
 curl -s -X POST $URL -d chat_id=$ID -d text="$output"
+sleep 30
+sudo find /backup_$(hostname)/ -mtime +3 -delete -and -print > /tmp/delete_log.txt
+
